baa33fcc4e94e248d521151580aab1af79c14365
EVCareeria <tuhohami@gmail.com>
2023-09-16 17:48:04 +0300

    Refactoring of react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 import {Portfolio} from "./pages/portfolio/Portfolio";
 import {Home} from "./pages/home/Home";
 import {About} from "./pages/about/About";
@@ -7,14 +7,16 @@ import {Navbar} from "./pages/navbar/Navbar";
 function App() {
   return (
       <div className="App">
-          < Navbar />
-        <Routes>
-          <Route path="HValokuvaus/" element={ <Home/> } />
-          <Route path="HValokuvaus/about" element={ <About/> } />
-          <Route path="HValokuvaus/portfolio" element={ <Portfolio/> } />
-        </Routes>
+          <Navbar />
+              <Routes>
+                  <Route path="/" element={ <Home/> } />
+                  <Route path="/about" element={ <About/> } />
+                  <Route path="/portfolio" element={ <Portfolio/> } />
+              </Routes>
       </div>
   )
 }
+
+
 
 export default App
